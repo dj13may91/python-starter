5f5311ef66c42a264e9c4163a955047f8367a65b
Divya Jain <djain@qualys.com>
2019-07-10 18:47:32 +0530

    more methods of python

--- venv/Demo.py
+++ venv/Demo.py
@@ -1,7 +1,12 @@
+import os
+import sys
+
 print("hello")
+print(os.path.dirname(sys.executable))
+
 for a in range(1, 10, 2):
     print(a)
-
+# pdb.setTrace()
 data = [1, 3, 2, 4]
 data.sort()
 print(data)
